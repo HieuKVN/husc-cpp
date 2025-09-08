39e34988a561ce520b066fa2e6c634fe431f242e
Trần Kiêm Công Hiếu <110919090+HieuKVN@users.noreply.github.com>
2025-09-08 22:51:07 +0700

    first commit

---
+++ pb0004.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long m, n;
+    cin >> m >> n;
+    cout << (m % 10 + n % 10) << endl;
+    cout << ((m / 1000 % 10) * (n / 1000 % 10)) << endl;
+    return 0;
+}
---
+++ pb0005.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    long long d = n / 3;
+    cout << d << endl;
+    return 0;
+}
